c569c806cc2fc7aa04ee40b194b2e303eafe46da
Marcus Dantas <marvinsd@gmail.com>
2025-07-09 14:36:57 -0300

    chore: add swagger

--- backend/database/data-source.ts
+++ backend/database/data-source.ts
@@ -5,9 +5,9 @@ export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: false,
-  entities: ['src/**/*.entity.ts'],
+  entities: ["src/**/*.entity.ts"],
 };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,25 @@
 import "dotenv/config";
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("Cinevault")
+    .setDescription("A web application for managing a list of movies.")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("swagger", app, document, {
+    customSiteTitle: "Cinevault - Swagger",
+    swaggerOptions: {
+      docExpansion: "none",
+    },
+  });
+
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
